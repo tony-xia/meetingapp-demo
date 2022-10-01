a1174e60e172b93deda60986e396251e28b2866c
Cyrstal <liufen_tj@hotmail.com>
2022-10-01 22:27:13 +1000

    Use file scoped namespace

--- MeetingAppDemo/ChatHub.cs
+++ MeetingAppDemo/ChatHub.cs
@@ -1,36 +1,35 @@
-﻿namespace MeetingAppDemo
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MeetingAppDemo;
+
+/// <summary>
+/// A SignalR Hub class
+/// </summary>
+public class ChatHub: Hub
 {
-    using System;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.SignalR;
-
     /// <summary>
-    /// A SignalR Hub class
+    /// Method to send the message to all clients.
     /// </summary>
-    public class ChatHub: Hub
+    /// <param name="message"></param>
+    /// <param name="status"></param>
+    /// <returns></returns>
+    public async Task SendMessage(string message, string status)
     {
-        /// <summary>
-        /// Method to send the message to all clients.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="status"></param>
-        /// <returns></returns>
-        public async Task SendMessage(string message, string status)
-        {
-            Console.WriteLine($"New Message: {message} {status}");
-            await Clients.All.SendAsync("ReceiveMessage", message, status);
-        }
+        Console.WriteLine($"New Message: {message} {status}");
+        await Clients.All.SendAsync("ReceiveMessage", message, status);
+    }
 
-        public override Task OnConnectedAsync()
-        {
-            Console.WriteLine("A client is connected");
-            return base.OnConnectedAsync();
-        }
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine("A client is connected");
+        return base.OnConnectedAsync();
+    }
 
-        public override Task OnDisconnectedAsync(Exception exception)
-        {
-            Console.WriteLine("A client is disconnected");
-            return base.OnDisconnectedAsync(exception);
-        }
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        Console.WriteLine("A client is disconnected");
+        return base.OnDisconnectedAsync(exception);
     }
 }
